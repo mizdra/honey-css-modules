345cad8687b5e2c63bb1f58e3a402e9d24a08fd1
mizdra <pp.mizdra@gmail.com>
2024-12-08 19:55:35 +0900

    feat: tsserver dynamically loads type definitions of CSS Modules

---
+++ packages/ts-honey-css-modules-plugin/src/language-plugin.ts
@@ -0,0 +1,55 @@
+import type { LanguagePlugin, VirtualCode } from '@volar/language-core';
+import type {} from '@volar/typescript';
+import type { ResolvedHCMConfig } from 'honey-css-modules';
+import { createDtsCode, parseCSSModuleCode, type Resolver } from 'honey-css-modules';
+import ts from 'typescript';
+
+const LANGUAGE_ID = 'css-module';
+
+export function createCSSModuleLanguagePlugin(
+  config: ResolvedHCMConfig,
+  resolver: Resolver,
+  isExternalFile: (filename: string) => boolean,
+): LanguagePlugin<string, VirtualCode> {
+  return {
+    getLanguageId(scriptId) {
+      // TODO: Handle only .module.css. Ignore other CSS files.
+      if (!scriptId.endsWith('.css')) return undefined;
+      return LANGUAGE_ID;
+    },
+    createVirtualCode(scriptId, languageId, snapshot) {
+      if (languageId !== LANGUAGE_ID) return undefined;
+
+      const length = snapshot.getLength();
+      const cssModule = snapshot.getText(0, length);
+      const cssModuleFile = parseCSSModuleCode(cssModule, { filename: scriptId, dashedIdents: config.dashedIdents });
+      const dtsCode = createDtsCode(cssModuleFile, { resolver, isExternalFile });
+      return {
+        id: 'main',
+        languageId: LANGUAGE_ID,
+        snapshot: {
+          getText: (start, end) => dtsCode.slice(start, end),
+          getLength: () => dtsCode.length,
+          getChangeRange: () => undefined,
+        },
+        mappings: [],
+      };
+    },
+    typescript: {
+      extraFileExtensions: [
+        {
+          extension: 'css',
+          isMixedContent: true,
+          scriptKind: ts.ScriptKind.TS,
+        },
+      ],
+      getServiceScript(root) {
+        return {
+          code: root,
+          extension: ts.Extension.Ts,
+          scriptKind: ts.ScriptKind.TS,
+        };
+      },
+    },
+  };
+}
