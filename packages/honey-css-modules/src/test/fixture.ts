b914c01938d7fb5b99e9f41dbf883a3172d21ef6
mizdra <pp.mizdra@gmail.com>
2024-12-29 13:31:14 +0900

    chore: use Faux ESM

--- packages/honey-css-modules/src/test/fixture.ts
+++ packages/honey-css-modules/src/test/fixture.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'node:crypto';
 import { tmpdir } from 'node:os';
 import { join } from 'node:path';
+// @ts-expect-error -- `require(esm)` is not supported by tsc, so ignore the error
 import { defineIFFCreator } from '@mizdra/inline-fixture-files';
 
 const fixtureDir = join(tmpdir(), 'honey-css-modules', process.env['VITEST_POOL_ID']!);
--- packages/ts-honey-css-modules-plugin/src/index.ts
+++ packages/ts-honey-css-modules-plugin/src/index.ts
@@ -1,11 +1,10 @@
-import type { createAsyncLanguageServicePlugin } from '@volar/typescript/lib/quickstart/createAsyncLanguageServicePlugin.js';
+import { createLanguageServicePlugin } from '@volar/typescript/lib/quickstart/createLanguageServicePlugin.js';
 import type { HCMConfig } from 'honey-css-modules';
 import { createIsExternalFile, createResolver, readConfigFile, resolveConfig } from 'honey-css-modules';
 import { ConfigNotFoundError } from 'honey-css-modules';
 import { createCSSModuleLanguagePlugin } from './language-plugin.js';
 
-type Create = Parameters<typeof createAsyncLanguageServicePlugin>[2];
-const create: Create = async (ts, info) => {
+const plugin = createLanguageServicePlugin((ts, info) => {
   if (info.project.projectKind !== ts.server.ProjectKind.Configured) {
     info.project.projectService.logger.info(`[ts-honey-css-modules-plugin] tsconfig.json not found`);
     return {
@@ -16,8 +15,7 @@ const create: Create = async (ts, info) => {
 
   let config: HCMConfig;
   try {
-    // eslint-disable-next-line @typescript-eslint/await-thenable -- TODO: remove await
-    config = await readConfigFile(cwd);
+    config = readConfigFile(cwd);
     info.project.projectService.logger.info(`[ts-honey-css-modules-plugin] Loaded config: ${JSON.stringify(config)}`);
   } catch (error) {
     // If the config file is not found, disable the plugin.
@@ -39,6 +37,6 @@ const create: Create = async (ts, info) => {
     //   info.languageService = proxyLanguageService(ts, language, info.languageService);
     // },
   };
-};
+});
 
-export default create;
+export = plugin;
