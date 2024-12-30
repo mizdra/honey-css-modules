5647f9aad1cc1121442aca15f93369443f920dc5
mizdra <pp.mizdra@gmail.com>
2024-12-30 23:38:18 +0900

    chore: update example

--- example/generated/src/a.module.css.d.ts
+++ example/generated/src/a.module.css.d.ts
@@ -1,5 +1,10 @@
 declare const styles: Readonly<
   & { a_1: string }
   & { a_2: string }
+  & { a_2: string }
+  & { a_3: string }
+  & (typeof import('./b.module.css'))['default']
+  & Pick<(typeof import('./c.module.css'))['default'], 'c_1'>
+  & { c_alias: (typeof import('./c.module.css'))['default']['c_2'] }
 >;
 export default styles;
---
+++ example/generated/src/c.module.css.d.ts
@@ -0,0 +1,5 @@
+declare const styles: Readonly<
+  & { c_1: string }
+  & { c_2: string }
+>;
+export default styles;
