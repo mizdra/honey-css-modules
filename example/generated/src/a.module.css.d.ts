7da1af869a11a121240bac468617ee52617bf7f0
mizdra <pp.mizdra@gmail.com>
2025-01-05 15:14:58 +0900

    chore: update example

--- example/generated/src/a.module.css.d.ts
+++ example/generated/src/a.module.css.d.ts
@@ -1,10 +1,10 @@
-declare const styles: Readonly<
-  & { a_1: string }
-  & { a_2: string }
-  & { a_2: string }
-  & { a_3: string }
-  & (typeof import('./b.module.css'))['default']
-  & Pick<(typeof import('./c.module.css'))['default'], 'c_1'>
-  & { c_alias: (typeof import('./c.module.css'))['default']['c_2'] }
->;
+declare const styles = {
+  a_1: '' as readonly string,
+  a_2: '' as readonly string,
+  a_2: '' as readonly string,
+  a_3: '' as readonly string,
+  ...(await import('./b.module.css')).default,
+  c_1: (await import('./c.module.css')).default.c_1,
+  c_alias: (await import('./c.module.css')).default.c_2,
+};
 export default styles;
