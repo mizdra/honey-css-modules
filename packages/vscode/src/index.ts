f8d57277a60342fe7a33472ead9a5721f930eee6
mizdra <pp.mizdra@gmail.com>
2025-01-14 21:59:00 +0900

    feat!: patch vscode.css-language-features to disable rename and references

--- packages/vscode/src/index.ts
+++ packages/vscode/src/index.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
 
+import fs from 'node:fs';
+import path from 'node:path';
 import * as vscode from 'vscode';
-import * as lsp from 'vscode-languageclient/node';
 
-let client: lsp.BaseLanguageClient;
-
-export async function activate(_context: vscode.ExtensionContext) {
+export function activate(_context: vscode.ExtensionContext) {
   console.log('[vscode-honey-css-modules] Activated');
 
   // By default, `vscode.typescript-language-features` is not activated when a user opens *.css in VS Code.
@@ -15,57 +14,38 @@ export async function activate(_context: vscode.ExtensionContext) {
     console.log('[vscode-honey-css-modules] Activating `vscode.typescript-language-features`');
     tsExtension.activate();
   }
-
-  // Both vscode.css-language-features and tsserver receive "rename" requests for *.css.
-  // If more than one Provider receives a "rename" request, VS Code will use one of them.
-  // In this case, vscode.css-language-features is used to rename. However, we do not want this.
-  // Without rename in tsserver, we cannot rename class selectors across *.css and *.ts.
-  //
-  // Also, VS Code seems to send "references" requests to both vscode.css-language-features
-  // and tsserver and merge the results of both. Thus, when a user executes "Find all references"
-  // on a class selector, the same class selector appears twice.
-  //
-  // To avoid this, we advise users to disable vscode.css-language-features.
-  //
-  // NOTE: It might be a good idea to dynamically monkey-patch vscode.css-language-features
-  //       so that vscode.css-language-features ignores *.module.css.
-  const cssExtension = vscode.extensions.getExtension('vscode.css-language-features');
-  if (cssExtension) {
-    vscode.window
-      .showWarningMessage(
-        'To use "honey-css-modules" extension, please disable "CSS Language Features" extension.',
-        'Show "CSS Language Features" extension',
-      )
-      .then((selected) => {
-        if (selected) {
-          vscode.commands.executeCommand('workbench.extensions.search', '@builtin css-language-features');
-        }
-      });
-  }
-
-  // TODO: Do not use Node.js API
-  const serverModulePath = require.resolve('honey-css-modules-language-server');
-
-  const serverOptions: lsp.ServerOptions = {
-    run: {
-      module: serverModulePath,
-      transport: lsp.TransportKind.ipc,
-      options: { execArgv: [] },
-    },
-    debug: {
-      module: serverModulePath,
-      transport: lsp.TransportKind.ipc,
-      options: { execArgv: ['--nolazy', `--inspect=${6009}`] },
-    },
-  };
-  const clientOptions: lsp.LanguageClientOptions = {
-    documentSelector: [{ language: 'css' }, { language: 'scss' }, { language: 'less' }],
-    initializationOptions: {},
-  };
-  client = new lsp.LanguageClient('vscode-honey-css-modules', 'vscode-honey-css-modules', serverOptions, clientOptions);
-  await client.start();
 }
 
-export function deactivate(): Thenable<unknown> | undefined {
-  return client?.stop();
+// Both vscode.css-language-features and tsserver receive "rename" requests for *.css.
+// If more than one Provider receives a "rename" request, VS Code will use one of them.
+// In this case, vscode.css-language-features is used to rename. However, we do not want this.
+// Without rename in tsserver, we cannot rename class selectors across *.css and *.ts.
+//
+// Also, VS Code seems to send "references" requests to both vscode.css-language-features
+// and tsserver and merge the results of both. Thus, when a user executes "Find all references"
+// on a class selector, the same class selector appears twice.
+//
+// To avoid this, we patch vscode.css-language-features to disable rename and references.
+const cssExtension = vscode.extensions.getExtension('vscode.css-language-features');
+if (cssExtension) {
+  const readFileSync = fs.readFileSync;
+  const serverDirectory = path.join(cssExtension.extensionPath, 'server');
+  console.log(serverDirectory);
+
+  // @ts-expect-error
+  fs.readFileSync = (...args) => {
+    if (typeof args[0] !== 'string') return readFileSync(...args);
+    console.log(args[0]);
+
+    if (path.dirname(args[0]) === serverDirectory) {
+      let text = readFileSync(...args).toString();
+      if (text.includes('referencesProvider:!0,') || text.includes('renameProvider:!0,')) {
+        text = text.replaceAll(',referencesProvider:!0,', ',referencesProvider:!1,');
+        text = text.replaceAll(',renameProvider:!0,', ',renameProvider:!1,');
+        console.log(`[vscode-honey-css-modules] Patched ${args[0]}`);
+      }
+      return text;
+    }
+    return readFileSync(...args);
+  };
 }
