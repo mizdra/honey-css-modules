3cab5468f011ff76d5c8ee2b3365bae0a1f8c5a1
mizdra <pp.mizdra@gmail.com>
2025-01-18 20:43:05 +0900

    feat: add no-missing-ts-file rule

--- packages/stylelint-plugin/src/index.ts
+++ packages/stylelint-plugin/src/index.ts
@@ -1,3 +1,4 @@
+import { noMissingTsFile } from './rules/no-missing-ts-file.js';
 import { noUnusedClassNames } from './rules/no-unused-class-names.js';
 
-export = [noUnusedClassNames];
+export = [noUnusedClassNames, noMissingTsFile];
