04cae6a6796bf93caca56aaa0f214a0f8d50bdb0
mizdra <pp.mizdra@gmail.com>
2025-02-02 22:28:36 +0900

    refactor: does not export `TOKEN_CONSUMER_PATTERN` from core

--- packages/stylelint-plugin/src/util.ts
+++ packages/stylelint-plugin/src/util.ts
@@ -1,5 +1,12 @@
 import fs from 'node:fs/promises';
-import { TOKEN_CONSUMER_PATTERN } from 'honey-css-modules-core';
+
+/**
+ * The syntax pattern for consuming tokens imported from CSS Module.
+ * @example `styles.foo`
+ */
+// TODO: Support `styles['foo']` and `styles["foo"]`
+// TODO: Support `otherNameStyles.foo`
+const TOKEN_CONSUMER_PATTERN = /styles\.([$_\p{ID_Start}][$\u200c\u200d\p{ID_Continue}]*)/gu;
 
 export async function readFile(path: string): Promise<string> {
   return fs.readFile(path, 'utf-8');
