59358d58990a176875666ca08b350f2b4c6b5c50
mizdra <pp.mizdra@gmail.com>
2025-02-02 23:07:58 +0900

    test: add test for circular import

--- packages/codegen/e2e/index.test.ts
+++ packages/codegen/e2e/index.test.ts
@@ -70,3 +70,59 @@ test('reports system error', async () => {
     "
   `);
 });
+
+test('generates .d.ts with circular import', async () => {
+  const iff = await createIFF({
+    'src/a.module.css': dedent`
+      @import './b.module.css';
+      .a1 { color: red; }
+    `,
+    'src/b.module.css': dedent`
+      @import './a.module.css';
+      .b1 { color: red; }
+    `,
+    'src/c.module.css': dedent`
+      @import './c.module.css';
+      .c1 { color: red; }
+    `,
+    'hcm.config.mjs': dedent`
+      export default {
+        pattern: 'src/**/*.module.css',
+        dtsOutDir: 'dist',
+      };
+    `,
+  });
+  const hcm = spawnSync('node', [binPath], {
+    cwd: iff.rootDir,
+    // MEMO: Suppress ExperimentalWarning output from `fs.promises.glob` and `path.matchesGlob`
+    // TODO: Remove `--no-warnings=ExperimentalWarning`
+    env: { ...process.env, NODE_OPTIONS: '--no-warnings=ExperimentalWarning' },
+  });
+  expect(hcm.error).toBeUndefined();
+  expect(hcm.stderr.toString()).toBe('');
+  expect(hcm.status).toBe(0);
+  expect(await readFile(iff.join('dist/src/a.module.css.d.ts'), 'utf-8')).toMatchInlineSnapshot(`
+    "declare const styles = {
+      a1: '' as readonly string,
+      ...(await import('./b.module.css')).default,
+    };
+    export default styles;
+    "
+  `);
+  expect(await readFile(iff.join('dist/src/b.module.css.d.ts'), 'utf-8')).toMatchInlineSnapshot(`
+    "declare const styles = {
+      b1: '' as readonly string,
+      ...(await import('./a.module.css')).default,
+    };
+    export default styles;
+    "
+  `);
+  expect(await readFile(iff.join('dist/src/c.module.css.d.ts'), 'utf-8')).toMatchInlineSnapshot(`
+    "declare const styles = {
+      c1: '' as readonly string,
+      ...(await import('./c.module.css')).default,
+    };
+    export default styles;
+    "
+  `);
+});
